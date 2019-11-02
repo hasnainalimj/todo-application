ed28f884628d34fef801b2e971dc5ac88b37584b
hasnainalimj <mjhasnain15@gmail.com>
2019-11-02 19:38:47 +0500

    add todo

--- app.js
+++ app.js
@@ -14,4 +14,35 @@ getTodos = () => {
   }
 };
 
-getTodos();
+addTodo = () => {
+  try {
+    const payload = {
+      description: "My Name Is Hasnain Ali",
+      isDelete: false,
+      status: "open",
+      title: "Hasnain Todo",
+      type: "MJ"
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    };
+
+    fetch(`${BASE_URL}/todo`, options)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log("data", data);
+      });
+  } catch (error) {
+    console.log("error while adding todo", error);
+  }
+};
+
+// getTodos();
+//addTodo();
